b3e664bde09b388d03117dc75ecbf7dd0e8f97b7
Juwon Kim <juwkim@c2r1s8.42seoul.kr>
2023-07-23 07:55:10 +0900

    mouse move add

--- bonus/source/update_bonus.c
+++ bonus/source/update_bonus.c
@@ -6,14 +6,15 @@
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 00:19:54 by juwkim            #+#    #+#             */
-/*   Updated: 2023/07/23 06:07:26 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/07/23 07:54:57 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "update_bonus.h"
 #include "event_bonus.h"
 
-static bool		rotate(const int rotation, double *const lookat);
+static bool		rotate(t_game *const game, const int rotation, \
+	double *const lookat);
 static bool		move(const t_key *const key, const char *const *const board, \
 	t_player *const p);
 static double	get_moving_direction(const t_key *const key, double lookat);
@@ -25,25 +26,39 @@ bool	update(t_game *const game, const t_key *const key)
 
 	if (key->esc == true)
 		destroy(game);
-	rotated = rotate(key->rotation, &game->player.lookat);
+	rotated = rotate(game, key->rotation, &game->player.lookat);
 	moved = move(key, (const char *const *const)game->map.board, \
 		&game->player);
 	return (rotated | moved);
 }
 
-static bool	rotate(const int rotation, double *const lookat)
+static bool	rotate(t_game *const game, const int rotation, double *const lookat)
 {
-	if (rotation == KEY_RELESED)
-		return (false);
-	if (rotation == KEY_LEFT)
-		*lookat += RT_SPEED;
-	else if (rotation == KEY_RIGHT)
-		*lookat -= RT_SPEED;
-	if (*lookat >= 2 * M_PI)
-		*lookat -= 2 * M_PI;
-	else if (*lookat < 0)
-		*lookat += 2 * M_PI;
-	return (true);
+	int		x;
+	int		y;
+	bool	rotated;
+
+	rotated = false;
+	mlx_mouse_get_pos(game->win, &x, &y);
+	if (0 <= y && y < WIN_HEIGHT && ((0 <= x && x < WIN_WIDTH / 3) || \
+		(WIN_WIDTH * 2 / 3 <= x && x < WIN_WIDTH)))
+	{
+		*lookat += (WIN_WIDTH / 2 - x) * RT_SPEED / WIN_HEIGHT;
+		rotated = true;
+	}
+	if (rotation != KEY_RELESED)
+	{
+		if (rotation == KEY_LEFT)
+			*lookat += RT_SPEED;
+		else if (rotation == KEY_RIGHT)
+			*lookat -= RT_SPEED;
+		if (*lookat >= 2 * M_PI)
+			*lookat -= 2 * M_PI;
+		else if (*lookat < 0)
+			*lookat += 2 * M_PI;
+		rotated = true;
+	}
+	return (rotated);
 }
 
 static bool	move(const t_key *const key, const char *const *const board, \
