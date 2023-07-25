f195da586ef467c940babb83e244837ff39f55de
juwkim <=>
2023-07-25 23:21:16 +0900

    test

--- mandatory/include/window.h
+++ mandatory/include/window.h
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 12:20:57 by juwkim            #+#    #+#             */
-/*   Updated: 2023/07/25 23:20:07 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/07/25 23:21:14 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@
 # include "config.h"
 
 void	init_window(t_window *const win, void *mlx);
-
 
 #endif // __WINDOW_H__
