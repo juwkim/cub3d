ad23ceb0345e6617c56a1848ee2345e31bdc79dc
juwkim <gimjuwon-ui-MacBookPro.local>
2023-08-08 04:03:57 +0900

    save

---
+++ mandatory/source/cub3d/cub3d_play.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d_play.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juwkim <juwkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 03:11:17 by juwkim            #+#    #+#             */
+/*   Updated: 2023/08/08 04:01:47 by juwkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	cub3d_play(t_cub3d *const cub3d)
+{
+	static bool	updated = true;
+
+	if (updated)
+		cub3d_render(cub3d);
+	updated = cub3d_update(cub3d);
+	return (0);
+}
