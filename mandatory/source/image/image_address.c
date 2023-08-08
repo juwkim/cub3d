541a258947029fbc56fcdfe95231fd5e24fd104e
Juwon Kim <juwkim@c2r1s1.42seoul.kr>
2023-08-08 21:41:34 +0900

    save

--- mandatory/source/image/image_address.c
+++ mandatory/source/image/image_address.c
@@ -1,23 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   image.h                                            :+:      :+:    :+:   */
+/*   image_address.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/26 13:40:57 by juwkim            #+#    #+#             */
-/*   Updated: 2023/08/08 21:34:57 by juwkim           ###   ########.fr       */
+/*   Created: 2023/08/08 21:30:08 by juwkim            #+#    #+#             */
+/*   Updated: 2023/08/08 21:35:07 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef IMAGE_H
-# define IMAGE_H
+#include "image.h"
 
-# include "cub3d.h"
+t_color	*image_address(const t_image *const img, const int i, const int j)
+{
+	const char	*addr = img->addr + i * img->len + j * (img->bpp / 8);
 
-bool	image_init(t_image *const img, void *mlx, int width, int height);
-bool	image_init_by_xpm_file(t_image *const img, void *mlx, char *xpm_file);
-void	image_destroy(const t_image *const img, void *mlx);
-t_color	*image_address(const t_image *const img, const int i, const int j);
-
-#endif // __IMAGE_H__
+	return ((t_color *)addr);
+}
--- mandatory/source/texture/texture_render.c
+++ mandatory/source/texture/texture_render.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   texture_render.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juwkim <juwkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 02:08:22 by juwkim            #+#    #+#             */
-/*   Updated: 2023/08/08 03:30:01 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/08/08 21:35:47 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "texture.h"
+#include "image.h"
 
 static void	texture_render_background(const t_texture *const tex, \
 	t_window *const win);
@@ -25,7 +26,6 @@ static void	texture_render_background(const t_texture *const tex, \
 {
 	int		i;
 	int		j;
-	char	*p;
 
 	j = 0;
 	while (j < WIN_WIDTH)
@@ -33,14 +33,12 @@ static void	texture_render_background(const t_texture *const tex, \
 		i = 0;
 		while (i < WIN_HEIGHT / 2)
 		{
-			p = win->img.addr + i * win->img.len + j * (win->img.bpp / 8);
-			*(t_color *)p = tex->bgcolor[T_CEILING - N_BGPAD];
+			*image_address(&win->img, i, j) = tex->bgcolor[T_CEILING - N_BGPAD];
 			++i;
 		}
 		while (i < WIN_HEIGHT)
 		{
-			p = win->img.addr + i * win->img.len + j * (win->img.bpp / 8);
-			*(t_color *)p = tex->bgcolor[T_FLOOR - N_BGPAD];
+			*image_address(&win->img, i, j) = tex->bgcolor[T_FLOOR - N_BGPAD];
 			++i;
 		}
 		++j;
