bed34b2f8035cdf08fb1ff3de4f0787b88c98c59
juwkim <gimjuwon-ui-MacBookPro.local>
2023-08-11 23:56:55 +0900

    save

--- mandatory/include/camera.h
+++ mandatory/include/camera.h
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 14:03:16 by juwkim            #+#    #+#             */
-/*   Updated: 2023/08/11 23:43:51 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/08/11 23:55:42 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 bool	camera_init(t_camera *const cam, const double aof);
 void	camera_destroy(const t_camera *const cam);
+bool	camera_update(t_cub3d *const cub3d);
 bool	camera_rotate_by_mouse(t_camera *const cam, const t_window *const win, \
 	const t_mouse *const mouse);
 bool	camera_rotate_by_key(t_camera *const cam, const t_key *const key);
--- mandatory/source/cub3d/cub3d_update.c
+++ mandatory/source/cub3d/cub3d_update.c
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 00:19:54 by juwkim            #+#    #+#             */
-/*   Updated: 2023/08/11 23:29:21 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/08/11 23:56:29 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,7 @@
 
 bool	cub3d_update(t_cub3d *const cub3d)
 {
-	bool	rotated_by_key;
-	bool	rotated_by_mouse;
-	bool	moved;
+	const bool	cam_updated = camera_update(cub3d);
 
-	if (cub3d->key.esc == true)
-		cub3d_destroy(cub3d);
-	rotated_by_key = camera_rotate_by_key(&cub3d->cam, &cub3d->key);
-	rotated_by_mouse = camera_rotate_by_mouse(&cub3d->cam, &cub3d->win, \
-		&cub3d->mouse);
-	moved = camera_move_by_key(&cub3d->cam, &cub3d->key, &cub3d->map, \
-		&cub3d->tex);
-	return (rotated_by_key | rotated_by_mouse | moved);
+	return (cam_updated);
 }
