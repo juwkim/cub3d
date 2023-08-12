9c5f6e2878e20579b78d5528aeebe56077731bf1
juwkim <gimjuwon-ui-MacBookPro.local>
2023-08-12 15:04:50 +0900

    save

--- mandatory/source/door/door_update.c
+++ mandatory/source/door/door_update.c
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 00:03:21 by juwkim            #+#    #+#             */
-/*   Updated: 2023/08/12 15:03:16 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/08/12 15:04:45 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 bool	door_update(t_cub3d *const cub3d)
 {
 	t_mouse *const	mouse = &cub3d->mouse;
-	static	int a = 0;
+	static int		a = 0;
 
 	if (mouse->left == true && cub3d->win.tex_id[mouse->y][mouse->x] == T_DOOR)
 		printf("DOOR %d\n", a++);
